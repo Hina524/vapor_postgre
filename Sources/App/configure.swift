c7983190da2ea30d9e0e2a7e546bdd2376c85879
Hina524 <hina15520@gmail.com>
2024-08-27 14:49:02 +0900

    first commit

---
+++ Sources/App/configure.swift
@@ -0,0 +1,40 @@
+import NIOSSL
+import Fluent
+import FluentPostgresDriver
+import Leaf
+import Vapor
+
+// configures your application
+public func configure(_ app: Application) async throws {
+    // uncomment to serve files from /Public folder
+    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+
+    app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
+        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
+        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+        database: Environment.get("DATABASE_NAME") ?? "vapor_database",
+        tls: .prefer(try .init(configuration: .clientDefault)))
+    ), as: .psql)
+
+    app.migrations.add(CreateTodo())
+
+    app.views.use(.leaf)
+
+        let corsConfiguration = CORSMiddleware.Configuration(
+            allowedOrigin: .all,
+            allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+            allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin]
+        )
+        let cors = CORSMiddleware(configuration: corsConfiguration)
+        app.middleware = .init()
+        // cors middleware should come before default error middleware using `at: .beginning`
+        app.middleware.use(cors, at: .beginning)
+        app.middleware.use(ErrorMiddleware.default(environment: app.environment))
+
+
+
+    // register routes
+    try routes(app)
+}
